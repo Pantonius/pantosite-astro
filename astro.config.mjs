a3e299f1750f680ee813cc614d56232e1dfc3b3c
Anton Pogrebnjak <contact@pantonius.dev>
2025-02-09 01:12:55 +0100

    Updated blog post definition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pantonius.dev',
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+	},
 	integrations: [mdx(), sitemap()],
 });
