cd99d221742f53b6b086854013a32c0b5c6ff898
Anton Pogrebnjak <contact@pantonius.dev>
2026-02-23 01:58:27 +0100

    Freiraum

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,21 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const bachelor = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/content/bachelor', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    part: z.number(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -16,7 +31,7 @@ const blog = defineCollection({
 });
 
 const projects = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  // Load Markdown and MDX files in the `src/content/projects/` directory.
   loader: glob({
     base: './src/content/projects', pattern: '**/*.{md,mdx}'
   }),
@@ -31,4 +46,4 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, projects };
+export const collections = { bachelor, blog, projects };
