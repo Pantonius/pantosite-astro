59e1df4436a5bfcb502d91b4f6d717b6ba265ae5
Anton Pogrebnjak <contact@pantonius.dev>
2026-05-03 00:53:30 +0200

    Updated rss feeds

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Pantosite';
-export const SITE_DESCRIPTION = 'The personal Blog of one Anton Pogrebnjak';
+export const SITE_DESCRIPTION = 'The personal website of Anton';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,22 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  const projects = await getCollection('projects');
+
+  let unifiedItems = posts.map((post) => ({
+    ...post.data,
+    link: `/blog/${post.id}/`,
+  })).concat(projects.map((project) => ({
+    ...project.data,
+    link: `/projects/${project.id}/`
+  })));
+  unifiedItems = unifiedItems.sort((a, b) => b.pubDate - a.pubDate);
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: unifiedItems,
+  });
 }
